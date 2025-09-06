4044cfb67de632b4ebcd6143a779e393dd5c29ea
Nathan Heller <nheller1@madisoncollege.edu>
2025-09-06 12:33:38 -0500

    Added second print line to main

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,6 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("This was branch B, which overwrote branch A and main");
-
+        System.out.println("This is me checking commit functionality in IntelliJ");
     }
 }
